725e398d77b8b8fbb86d37c3dfd2486f6e8e820a
Shai Wininger <shai.wininger@gmail.com>
2011-12-11 16:21:03 +0200

    added download url and additiona fuctionality

--- lib/cloudkey/media.rb
+++ lib/cloudkey/media.rb
@@ -56,5 +56,18 @@ module Cloudkey
 
       opts
     end
+
+    def download_url id, asset_name="mp4_h264_aac", security_level=SecurityPolicy.new, cdn_url='http://cdn.dmcloud.net'
+      url = "#{cdn_url}/route/http/#{@api.user_id}/#{id}/#{asset_name}.#{asset_name.split('_')[0]}"
+      API.sign_url url, @api.key, security_level
+    end
+
+    def url_components id, asset_name="mp4_h264_aac", security_level=SecurityPolicy.new, cdn_url='http://cdn.dmcloud.net'
+      {:cdn_url => cdn_url, :user_id => @api.user_id, :id => id, :asset_name => asset_name}
+    end
+
+    def sign_url(url)
+      API.sign_url url, @api.key, security_level
+    end
   end
 end
